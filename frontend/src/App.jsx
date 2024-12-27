8485711e2b49e8cde5a57a52b9c7915390e6edc0
nxtnilesh <nxtnilesh@gmail.com>
2024-12-27 12:54:46 +0530

    add daisyUI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-red-600">
+      <h1 className="text-3xl font-bold underline bg-red-900">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
